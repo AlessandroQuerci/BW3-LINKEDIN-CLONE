df4b54af696bdacf184809ba203026d9b93978ba
Alessandro Coretti <alessandrocoretti2002@gmail.com>
2024-12-17 19:01:32 +0100

    fifth commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, ButtonGroup, Col, Container, Dropdown, DropdownButton, Row } from "react-bootstrap";
 
 //ICONE
 import { LuGitCompareArrows } from "react-icons/lu";
+import ProfileModal from "./ProfileModal";
 
 const Home = () => {
   return (
@@ -26,7 +27,7 @@ const Home = () => {
                     className=" rounded-circle"
                   />
                 </div>
-                <Button className=" rounded-pill bg-white text-start w-100 ">Crea un post</Button>
+                <ProfileModal />
               </div>
               <div className="d-flex justify-content-around mt-3">
                 <Button size="sm d-flex justify-content-between align-items-center gap-2 border-0 bg-white text-secondary fw-bold">
@@ -50,7 +51,35 @@ const Home = () => {
                 </Button>
               </div>
             </div>
-            <div id="user-box" className="mt-3 bg-white rounded p-2 position-relative">
+            <div className="d-flex align-items-center">
+              <hr className="w-25 me-5" />
+              <p className="m-0 fs-7">Seleziona la visualizzazione del feed:</p>
+
+              <Dropdown className="h-28">
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  <strong> Più rilevanti</strong>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu className="bg-white text-black">
+                  <Dropdown.Item href="#/action-1" className="p-2">
+                    <strong>
+                      <p className="m-0 fs-6">Più rilevanti</p>
+                    </strong>
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-2" className="p-2">
+                    <strong>
+                      <p className="m-0 fs-6">Più recenti</p>
+                    </strong>
+                  </Dropdown.Item>
+                  <p className="m-0 fs-7 px-2">
+                    Questa opzione influirà soltanto sulla visualizzazione del tuo feed attuale su questo dispositivo. Puoi modificare la visualizzazione
+                    predefinita nelle Impostazioni.
+                  </p>
+                </Dropdown.Menu>
+              </Dropdown>
+            </div>
+
+            <div id="user-box" className=" bg-white rounded p-2 position-relative">
               <div className="d-flex align-items-center gap-1 ">
                 <img
                   src="https://images.unsplash.com/photo-1467010234262-77bada75a47d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHBlcnNvbmF8ZW58MHwxfDB8fHww"
@@ -161,10 +190,99 @@ const Home = () => {
               </div>
               <div className="d-flex justify-content-around mt-3">
                 <Button className="d-flex align-items-center gap-1 p-0 m-0 bg-transparent border-0 text-secondary fw-bold">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-hand-thumbs-up" viewBox="0 0 16 16">
-                    <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
-                  </svg>
-                  <p className="m-0 fs-6 d-none d-lg-block">Consiglia</p>
+                  {["up"].map((direction) => (
+                    <DropdownButton
+                      as={ButtonGroup}
+                      key={direction}
+                      id={`dropdown-button-drop-${direction}`}
+                      drop={direction}
+                      variant="secondary"
+                      title={
+                        <div className="d-flex">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="18"
+                            fill="currentColor"
+                            className="bi bi-hand-thumbs-up"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
+                          </svg>
+                          <p className="m-0 fs-6 d-none d-lg-block">Consiglia</p>
+                        </div>
+                      }
+                    >
+                      <div className="d-flex gap-1  rounded-pill bg-white p-1 mb-1">
+                        <Dropdown.Item eventKey="1" className="p-0">
+                          <img
+                            className="reactions-icon reactions-menu__icon"
+                            src="https://static.licdn.com/aero-v1/sc/h/8fz8rainn3wh49ad6ef9gotj1"
+                            alt="like"
+                            data-test-reactions-icon-type="LIKE"
+                            data-test-reactions-icon-theme="light"
+                            data-test-reactions-icon-style="consumption"
+                            data-test-reactions-icon-size="large"
+                          />
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey="2" className="p-0">
+                          <img
+                            className="reactions-icon reactions-menu__icon reactions-icon__consumption--large data-test-reactions-icon-type-PRAISE data-test-reactions-icon-theme-light"
+                            src="https://static.licdn.com/aero-v1/sc/h/1mbfgcprj3z93pjntukfqbr8y"
+                            alt="celebrate"
+                            data-test-reactions-icon-type="PRAISE"
+                            data-test-reactions-icon-theme="light"
+                            data-test-reactions-icon-style="consumption"
+                            data-test-reactions-icon-size="large"
+                          />
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey="3" className="p-0">
+                          <img
+                            className="reactions-icon reactions-menu__icon reactions-icon__consumption--large data-test-reactions-icon-type-APPRECIATION data-test-reactions-icon-theme-light"
+                            src="https://static.licdn.com/aero-v1/sc/h/cv29x2jo14dbflduuli6de6bf"
+                            alt="support"
+                            data-test-reactions-icon-type="APPRECIATION"
+                            data-test-reactions-icon-theme="light"
+                            data-test-reactions-icon-style="consumption"
+                            data-test-reactions-icon-size="large"
+                          />
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey="4" className="p-0">
+                          <img
+                            className="reactions-icon reactions-menu__icon reactions-icon__consumption--large data-test-reactions-icon-type-EMPATHY data-test-reactions-icon-theme-light"
+                            src="https://static.licdn.com/aero-v1/sc/h/6f5qp9agugsqw1swegjxj86me"
+                            alt="love"
+                            data-test-reactions-icon-type="EMPATHY"
+                            data-test-reactions-icon-theme="light"
+                            data-test-reactions-icon-style="consumption"
+                            data-test-reactions-icon-size="large"
+                          />
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey="5" className="p-0">
+                          <img
+                            className="reactions-icon reactions-menu__icon reactions-icon__consumption--large data-test-reactions-icon-type-INTEREST data-test-reactions-icon-theme-light"
+                            src="https://static.licdn.com/aero-v1/sc/h/9ry9ng73p660hsehml9i440b2"
+                            alt="insightful"
+                            data-test-reactions-icon-type="INTEREST"
+                            data-test-reactions-icon-theme="light"
+                            data-test-reactions-icon-style="consumption"
+                            data-test-reactions-icon-size="large"
+                          />
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey="6" className="p-0">
+                          <img
+                            className="reactions-icon reactions-menu__icon reactions-icon__consumption--large data-test-reactions-icon-type-ENTERTAINMENT data-test-reactions-icon-theme-light"
+                            src="https://static.licdn.com/aero-v1/sc/h/qye2jwjc8dw20nuv6diudrsi"
+                            alt="funny"
+                            data-test-reactions-icon-type="ENTERTAINMENT"
+                            data-test-reactions-icon-theme="light"
+                            data-test-reactions-icon-style="consumption"
+                            data-test-reactions-icon-size="large"
+                          />
+                        </Dropdown.Item>
+                      </div>
+                    </DropdownButton>
+                  ))}
                 </Button>
 
                 <Button className="d-flex align-items-center gap-1 p-0 m-0 bg-transparent border-0 text-secondary fw-bold">
