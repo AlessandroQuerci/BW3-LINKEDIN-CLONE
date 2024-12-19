c4bef3c884f5c5e97a7f632d5fe16acd94166a04
annunziatadr99 <anudar99@hotmail.it>
2024-12-19 18:21:44 +0100

    aggiunta card esoerienze, competenze e formazione

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,11 +1,15 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import userEmpty from "../assets/image/empty-user.png";
 import userNoPhoto from "../assets/image/no-photo.png";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useDispatch, useSelector } from "react-redux";
+import { IoMdClose } from "react-icons/io";
+import { FaBriefcase } from "react-icons/fa";
+import { LuSchool } from "react-icons/lu";
+
 //MEDIA
 import littleHouse from "../assets/image/little-house.png";
 import littleCamera from "../assets/image/little-camera.png";
@@ -157,20 +161,37 @@ const ProfilePage = () => {
           <Col xs={5}>
             <div className="d-flex flex-column bg-white border border-1 rounded-3 mb-2">
               <div id="userField">
-                <img src={userEmpty} alt="" className="img-fluid object-fit-cover rounded-top-3 w-100" />
-                <Button className="p-2 bg-white border-0 z-1 rounded-circle d-flex justify-content-center" id="btn-camera">
+                <img
+                  src={userEmpty}
+                  alt=""
+                  className="img-fluid object-fit-cover rounded-top-3 w-100"
+                />
+                <Button
+                  className="p-2 bg-white border-0 z-1 rounded-circle d-flex justify-content-center"
+                  id="btn-camera"
+                >
                   <FaCamera className="text-success" />
                 </Button>
               </div>
               <div className="mb-4" id="userNoPhoto">
-                <Button className="rounded-circle bg-transparent border-0 " id="addPhoto">
-                  <img src={userImage} alt="" className="img-fluid object-fit-cover rounded-circle " id="userImage" />
+                <Button
+                  className="rounded-circle bg-transparent border-0 "
+                  id="addPhoto"
+                >
+                  <img
+                    src={userImage}
+                    alt=""
+                    className="img-fluid object-fit-cover rounded-circle "
+                    id="userImage"
+                  />
                 </Button>
               </div>
               <div className=" d-flex justify-content-start flex-column mx-4">
                 <div className="d-flex justify-content-between align-items-center">
                   <div className=" d-flex justify-content-start align-items-center">
-                    <h4 className="h4  me-2 mb-0">{userName + " " + userSurname}</h4>
+                    <h4 className="h4  me-2 mb-0">
+                      {userName + " " + userSurname}
+                    </h4>
                     <Button className="rounded-pill bg-white text-linkedin fw-bold fs-7 p-0 px-2 d-flex justify-content-center align-items-center blueBtns ">
                       <IoShieldCheckmarkOutline className="fs-6 me-1" />
                       Aggiungi badge di verifica
@@ -218,7 +239,8 @@ const ProfilePage = () => {
                         <p className="m-0 ms-2">In quale settore lavori?</p>
                       </div>
                       <p className="text-secondary fs-7 mb-4  fw-bold ">
-                        Gli utenti che aggiungono un settore ricevono fino a 2,5 volte più visualizzazioni del profilo.
+                        Gli utenti che aggiungono un settore ricevono fino a 2,5
+                        volte più visualizzazioni del profilo.
                       </p>
                       <Button className="rounded-pill bg-white text-secondary fw-bold fs-6 py-1 px-2 d-flex justify-content-center align-items-center me-2 border-secondary blackBtns ">
                         Aggiungi settore
@@ -229,10 +251,14 @@ const ProfilePage = () => {
                     <div className="border border-1 rounded px-3 py-3 me-3">
                       <div className="d-flex justify-content-start align-items-center mb-3">
                         <img src={littleCamera} alt="" />
-                        <p className="m-0 ms-2">Aggiungi una foto al tuo profilo per aiutare gli altri a riconoscerti</p>
+                        <p className="m-0 ms-2">
+                          Aggiungi una foto al tuo profilo per aiutare gli altri
+                          a riconoscerti
+                        </p>
                       </div>
                       <p className="text-secondary fs-7 mb-4  fw-bold ">
-                        Gli utenti con una foto del profilo ricevono fino a 2,3 volte più visualizzazioni del profilo.
+                        Gli utenti con una foto del profilo ricevono fino a 2,3
+                        volte più visualizzazioni del profilo.
                       </p>
                       <Button className="rounded-pill bg-white text-secondary fw-bold fs-6 py-1 px-2 d-flex justify-content-center align-items-center me-2 border-secondary  blackBtns">
                         Aggiungi foto
@@ -243,10 +269,14 @@ const ProfilePage = () => {
                     <div className="border border-1 rounded px-3 py-3 me-3">
                       <div className="d-flex justify-content-start align-items-center mb-3">
                         <img src={littleWindow} alt="" />
-                        <p className="m-0 ms-2 text-truncate">Scrivi un riepilogo per mettere in evidenza la tua personalità o la tua esperienza lavorativa</p>
+                        <p className="m-0 ms-2 text-truncate">
+                          Scrivi un riepilogo per mettere in evidenza la tua
+                          personalità o la tua esperienza lavorativa
+                        </p>
                       </div>
                       <p className="text-secondary fs-7 mb-4  fw-bold ">
-                        Gli utenti che includono un riepilogo ricevono fino a 3,9 volte più visualizzazioni del profilo.
+                        Gli utenti che includono un riepilogo ricevono fino a
+                        3,9 volte più visualizzazioni del profilo.
                       </p>
                       <Button className="rounded-pill bg-white text-secondary fw-bold fs-6 py-1 px-2 d-flex justify-content-center align-items-center me-2 border-secondary blackBtns">
                         Aggiungi un riepilogo
@@ -268,8 +298,12 @@ const ProfilePage = () => {
                       <div className="d-flex justify-content-start align-items-top">
                         <BsFillPeopleFill className="fs-3 me-2" />
                         <div className="d-flex flex-column">
-                          <p className="fw-bold m-0 fs-6 ">3 visualizzazioni del profilo</p>
-                          <p className="fs-7">Scopri chi ha visitato il tuo profilo</p>
+                          <p className="fw-bold m-0 fs-6 ">
+                            3 visualizzazioni del profilo
+                          </p>
+                          <p className="fs-7">
+                            Scopri chi ha visitato il tuo profilo
+                          </p>
                         </div>
                       </div>
                     </Col>
@@ -277,8 +311,12 @@ const ProfilePage = () => {
                       <div className="d-flex justify-content-start align-items-top">
                         <IoStatsChartSharp className="fs-3 me-2" />
                         <div className="d-flex flex-column">
-                          <p className="fw-bold m-0 fs-6 ">0 impressioni del post</p>
-                          <p className="fs-7 m-0">Crea un post per aumentare l'interesse</p>
+                          <p className="fw-bold m-0 fs-6 ">
+                            0 impressioni del post
+                          </p>
+                          <p className="fs-7 m-0">
+                            Crea un post per aumentare l'interesse
+                          </p>
                           <p className="fs-7 text-secondary">Ultimi 7 giorni</p>
                         </div>
                       </div>
@@ -295,7 +333,10 @@ const ProfilePage = () => {
                 <div className="d-flex justify-content-between align-items-top mb-3">
                   <div className="pt-2">
                     <h3 className="fs-5 m-0 ">Attività</h3>
-                    <a href="#" className="text-primary d-flex justify-content-start align-items-center text-decoration-none">
+                    <a
+                      href="#"
+                      className="text-primary d-flex justify-content-start align-items-center text-decoration-none"
+                    >
                       0 follower
                     </a>
                   </div>
@@ -308,12 +349,121 @@ const ProfilePage = () => {
                     </Button>
                   </div>
                 </div>
-                <p className="text-black m-0 ">Non hai ancora pubblicato nulla</p>
-                <p className="text-secondary  fs-7 fw-light mb-2">I posti che condividi appariranno qui</p>
+                <p className="text-black m-0 ">
+                  Non hai ancora pubblicato nulla
+                </p>
+                <p className="text-secondary  fs-7 fw-light mb-2">
+                  I posti che condividi appariranno qui
+                </p>
               </div>
               <Button className="border-0 bg-transparent text-secondary w-100 rounded-0 rounded-bottom border-top p-0 m-0 py-2 d-flex justify-content-center align-items-center  fw-bold showBtns">
-                Mostra tutte le attività <FaLongArrowAltRight className="ms-1" />
+                Mostra tutte le attività{" "}
+                <FaLongArrowAltRight className="ms-1" />
               </Button>
+            </div>
+            <div className="d-flex flex-column bg-white border border-1 rounded-3  mb-2 dashed-border">
+              <div className="px-3 pt-2">
+                <div className="d-flex justify-content-between align-items-top mb-3">
+                  <div className="pt-2">
+                    <h3 className="fs-5 m-0 text-secondary">Esperienza</h3>
+                    <p className="fs-7 fw-light mb-2">
+                      Metti in risalto i risultati raggiunti e ottieni fino a 2
+                      volte più visualizzazioni del profilo e collegamenti
+                    </p>
+                    <div className="d-flex  justify-content-start align-items-center mb-3">
+                      <FaBriefcase className="fs-1 p-2 text-secondary border  border-black border-1 rounded " />
+                      <div className="d-flex flex-column">
+                        <p className="m-0 ms-2 fs-6 text-secondary ">
+                          Qualifica
+                        </p>
+                        <p className="m-0 ms-2 fs-8 text-secondary ">
+                          Organizzazione
+                        </p>
+                        <p className="m-0 ms-2 fs-8 text-secondary ">
+                          2023 - Presente
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className=" d-flex justify-content-end align-items-center">
+                    <Button className="border-0 bg-transparent rounded-circle d-flex justify-content-center align-items-center p-3 pencilBtns">
+                      <IoMdClose className="fs-5 text-black  " />
+                    </Button>
+                  </div>
+                </div>
+                <Button className="rounded-pill bg-white text-linkedin fw-bold fs-6 py-1 d-flex justify-content-center align-items-center me-2 mb-4 border-primary blueBtns ">
+                  Aggiungi un esperienza
+                </Button>
+              </div>
+            </div>
+            <div className="d-flex flex-column bg-white border border-1 rounded-3  mb-2 dashed-border">
+              <div className="px-3 pt-2">
+                <div className="d-flex justify-content-between align-items-top mb-3">
+                  <div className="pt-2">
+                    <h3 className="fs-5 m-0 text-secondary">Formazione</h3>
+                    <p className="fs-7 fw-light mb-2">
+                      Mostrando le tue qualifiche avrai fino a 2 volte più
+                      probabilità di ricevere in messaggio in Mail da recrutier
+                    </p>
+                    <div className="d-flex  justify-content-start align-items-center mb-3">
+                      <LuSchool className="fs-1 p-2 text-secondary border  border-black border-1 rounded " />
+                      <div className="d-flex flex-column">
+                        <p className="m-0 ms-2 fs-6 text-secondary ">
+                          Scuola o Università
+                        </p>
+                        <p className="m-0 ms-2 fs-8 text-secondary ">
+                          Laura, Campo di studio
+                        </p>
+                        <p className="m-0 ms-2 fs-8 text-secondary ">
+                          2019 - 2023
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className=" d-flex justify-content-end align-items-center">
+                    <Button className="border-0 bg-transparent rounded-circle d-flex justify-content-center align-items-center p-3 pencilBtns">
+                      <IoMdClose className="fs-5 text-black  " />
+                    </Button>
+                  </div>
+                </div>
+                <Button className="rounded-pill bg-white text-linkedin fw-bold fs-6 py-1 d-flex justify-content-center align-items-center me-2 mb-4 border-primary blueBtns ">
+                  Aggiungi un titolo di studio
+                </Button>
+              </div>
+            </div>
+
+            <div className="d-flex flex-column bg-white border border-1 rounded-3  mb-2 dashed-border mb-5">
+              <div className="px-3 pt-2">
+                <div className="d-flex justify-content-between align-items-top mb-3">
+                  <div className="pt-2">
+                    <h3 className="fs-5 m-0 text-secondary">Competenze</h3>
+                    <p className="fs-7 fw-light mb-2">
+                      Fai capire se hai un profilo adatto per le nuove
+                      opportunità: il 50% dei recruiter usa i dati sulle
+                      competenze per coprire le posizioni aperte
+                    </p>
+                    <div className="d-flex  justify-content-start align-items-center mb-3">
+                      <div className="d-flex flex-column">
+                        <p className="m-0 ms-2 fs-6 text-secondary ">
+                          Soft Skills
+                        </p>
+                        <hr />
+                        <p className="m-0 ms-2 fs-6 text-secondary ">
+                          Competenze tecniche
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className=" d-flex justify-content-end align-items-center">
+                    <Button className="border-0 bg-transparent rounded-circle d-flex justify-content-center align-items-center p-3 pencilBtns">
+                      <IoMdClose className="fs-5 text-black  " />
+                    </Button>
+                  </div>
+                </div>
+                <Button className="rounded-pill bg-white text-linkedin fw-bold fs-6 py-1 d-flex justify-content-center align-items-center me-2 mb-4 border-primary blueBtns ">
+                  Aggiungi competenze
+                </Button>
+              </div>
             </div>
           </Col>
           <Col xs={2}>
@@ -332,7 +482,9 @@ const ProfilePage = () => {
                   <ImPencil className="fs-5 text-black  " />
                 </Button>
               </div>
-              <p className="text-secondary fw-light mb-3 ">www.linkedin.com/in/alessandro-querci-887a28239</p>
+              <p className="text-secondary fw-light mb-3 ">
+                www.linkedin.com/in/alessandro-querci-887a28239
+              </p>
             </div>
             <div className="sticky-top">
               <img src={hiring} alt="" className="w-100 rounded sticky-top" />
